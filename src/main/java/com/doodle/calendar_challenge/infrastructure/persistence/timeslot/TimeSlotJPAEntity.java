9d75fb0824fda6f07a8b9d2e6493ca63a5c6b2d3
firefarts <leafar.zepol.85@gmail.com>
2026-05-12 08:24:43 +0200

    Adding indexes to improve query performance

--- src/main/java/com/doodle/calendar_challenge/infrastructure/persistence/timeslot/TimeSlotJPAEntity.java
+++ src/main/java/com/doodle/calendar_challenge/infrastructure/persistence/timeslot/TimeSlotJPAEntity.java
@@ -2,6 +2,7 @@ package com.doodle.calendar_challenge.infrastructure.persistence.timeslot;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.Index;
 import jakarta.persistence.Table;
 import jakarta.persistence.Version;
 import lombok.AccessLevel;
@@ -13,7 +14,10 @@ import java.time.Instant;
 import java.util.UUID;
 
 @Entity
-@Table(name = "time_slots")
+@Table(name = "time_slots", indexes = {
+        @Index(name = "idx_ts_owner_start_end", columnList = "owner, startAt, endAt"),
+        @Index(name = "idx_ts_owner_busy_start_end", columnList = "owner, busy, startAt, endAt")
+})
 @Getter
 @Setter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
